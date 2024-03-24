5faf910f86b7076617bb22a262636abb3f7d5f2a
Martim Moniz <eloy.mtm@gmail.com>
2024-03-24 00:12:23 +0000

    Menu started and not neccessary argument (graph) removed from functions in Dataset

---
+++ classes/Menu.cpp
@@ -0,0 +1,84 @@
+//
+// Created by eloy_mm2004 on 23-03-2024.
+//
+
+#include "Menu.h"
+using namespace std;
+
+
+
+void Menu::run(){
+    Dataset objDataset = Dataset();
+
+}
+void Menu::clearScreen(){
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+}
+void Menu::mainMenu(Dataset &objDataset) {
+
+    int choice;
+
+    clearScreen(); // Limpa a tela antes de exibir o menu
+    cout << "\033[1;32m"; // Define a cor verde brilhante para o texto
+    cout << "==============================" << endl;
+    cout << "|       ** Main Menu **      |" << endl;
+    cout << "==============================" << endl;
+    cout << "|   1. MaxFlow               |" << endl;
+    cout << "|   2. IsWaterSufficient     |" << endl;
+    cout << "|   3. Opção 3               |" << endl;
+    cout << "|   4. Sair                  |" << endl;
+    cout << "==============================" << endl;
+    cout << "\033[0m"; // Resetar a cor do texto
+
+    cout << "Choose one option: \n";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            maxFlowMenu(objDataset);
+            break;
+        }
+        case 2:{
+            isWaterSufficientMenu(objDataset);
+            break;
+        }
+    }
+}
+
+void Menu::maxFlowMenu(Dataset &objDataset) {
+
+    int choice;
+
+    clearScreen(); // Limpa a tela antes de exibir o menu
+    cout << "\033[1;32m"; // Define a cor verde brilhante para o texto
+    cout << "==================================" << endl;
+    cout << "|     ** Max Flow Menu **         |" << endl;
+    cout << "==================================" << endl;
+    cout << "|   1. To a Target                |" << endl;
+    cout << "|   2. From a source to a target  |" << endl;
+    cout << "|   3. Best Max Flow              |" << endl;
+    cout << "|   4. Sair                       |" << endl;
+    cout << "==================================" << endl;
+    cout << "\033[0m"; // Resetar a cor do texto
+
+
+    switch (choice) {
+        case 1:{
+
+            break;
+        }
+    }
+
+
+}
+void Menu::wait(Dataset &objDataset) {
+    string o;
+    cout << "Press ENTER to continue...";
+    cin.get(); // Aguarda a entrada de uma tecla
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    mainMenu(objDataset);
+}
---
+++ classes/Menu.h
@@ -0,0 +1,19 @@
+
+#ifndef G03_1_MENU_H
+#define G03_1_MENU_H
+
+#include "Dataset.h"
+#include <limits>
+#include
+
+class Menu {
+    void run();
+    void clearScreen();
+    void mainMenu(Dataset &objDataset);
+    void maxFlowMenu(Dataset &objDataset);
+    void wait(Dataset &objDataset);
+    void isWaterSufficientMenu(Dataset &objDataset);
+};
+
+
+#endif //G03_1_MENU_H
